d3f69b6adb92d4338a0b6fc404aceed5096cf7fe
M-Lenvik <lenvik.marie@gmail.com>
2025-03-12 19:18:39 +0100

    new vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,13 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
-'base': '/Arbetsmetodik-Vue/'
+  plugins: [vue()],
+  base: '/Arbetsmetodik-Vue/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
